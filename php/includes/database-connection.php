75625a03796813aa40577d01372e26158c2c5bf2
Vinicius A Souza <90021919+xLupus@users.noreply.github.com>
2022-08-31 22:57:32 -0300

    Update database-connection.php

--- php/includes/database-connection.php
+++ php/includes/database-connection.php
@@ -1,37 +1,13 @@
 <?php
 
 //Casa Vini
-$DB_HOST = 'DESKTOP-8P375TU\\SQLEXPRESS';
-$DB_DRIVER = 'sqlsrv';
-$DB_NAME = 'BookComments';
-$DB_USER = 'sa';
-$DB_PASS = 'SL-221B-VAS';
+$DB_HOST = '';
+$DB_DRIVER = '';
+$DB_NAME = '';
+$DB_USER = '';
+$DB_PASS = '';
 
 $pdoConfig  = "$DB_DRIVER:Server=$DB_HOST;Database=$DB_NAME";
 
 $database = new PDO($pdoConfig,$DB_USER,$DB_PASS);
 
-//Casa Lucca
-/*
-$DB_HOST = 'DESKTOP-CQC2LSI\SQLSERVER'; 
-$DB_DRIVER = 'sqlsrv';
-$DB_NAME = 'BookComments';
-$DB_USER = 'sa';
-$DB_PASS = 'SL-221B-VAS';
-
-$pdoConfig  = "$DB_DRIVER:Server=$DB_HOST;Database=$DB_NAME";
-
-$database = new PDO($pdoConfig,NULL,NULL);
-*/
-//Senac
-/*
-$DB_HOST = '10.135.0.53\\sqledutsi';
-$DB_DRIVER = 'sqlsrv';
-$DB_NAME = 'ONU';
-$DB_USER = 'ALUNO';
-$DB_PASS = 'aluno';
-
-$pdoConfig  = "$DB_DRIVER:Server=$DB_HOST;Database=$DB_NAME";
-
-$database = new PDO($pdoConfig,$DB_USER,$DB_PASS);
-*/
