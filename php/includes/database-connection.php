dc708da706dec41d225b67ccca8561bfa04a8943
Vinicius A Souza <vinicius.as.dev@outlook.com>
2022-10-03 03:29:14 -0300

    Update database-connection.php

--- php/includes/database-connection.php
+++ php/includes/database-connection.php
@@ -1,13 +1,12 @@
 <?php
 
 //Casa Vini
-$DB_HOST = '';
+$DB_HOST   = '';
 $DB_DRIVER = '';
-$DB_NAME = '';
-$DB_USER = '';
-$DB_PASS = '';
+$DB_NAME   = '';
+$DB_USER   = '';
+$DB_PASS   = '';
 
-$pdoConfig  = "$DB_DRIVER:Server=$DB_HOST;Database=$DB_NAME";
-
-$database = new PDO($pdoConfig,$DB_USER,$DB_PASS);
+$pdoConfig = "$DB_DRIVER:Server=$DB_HOST;Database=$DB_NAME";
+$database  = new PDO($pdoConfig,$DB_USER,$DB_PASS);
 
