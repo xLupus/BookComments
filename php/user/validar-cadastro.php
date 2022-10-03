d5cd4cfb05bd77ac5c7c0d9f3eaf87414d67f74f
Vinicius A Souza <vinicius.as.dev@outlook.com>
2022-10-03 03:21:50 -0300

    Update validar-cadastro.php

--- php/user/validar-cadastro.php
+++ php/user/validar-cadastro.php
@@ -13,7 +13,6 @@ if(isset($_POST['btn_cadastrar'])){
     $senha = trim($_POST['senha'], '');
     $erros = [];
 
-
     if(!empty($nome)){
         $nome = htmlspecialchars($nome, ENT_COMPAT, 'UTF-8');    
     }else{$erros["NOME"] = "Preencha o campo";}
@@ -24,23 +23,18 @@ if(isset($_POST['btn_cadastrar'])){
     }else{$erros["EMAIL"] = "Preencha o campo";}
 
 
-    if(empty($senha)){
+    if(empty($senha))
         $erros["SENHA"] = "Preencha o campo";
-    }
-
 
     if(empty($erros)){
         $senha = password_hash($senha, PASSWORD_BCRYPT);
-
 
         $checagem = $database->prepare('SELECT email FROM BK_tbUsuario WHERE email = :email');
         $checagem->bindParam(':email', $email);
         $checagem->execute();
         $c = $checagem->fetch(PDO::FETCH_ASSOC);
         
-
-        if($c == false){ //Se o email n existir no BD
-            
+        if($c == false){ //Se o email n existir no BD     
             $stmt = $database->prepare("INSERT INTO BK_tbUsuario (nome, email, senha, permissao)
                                         VALUES (:nome, :email, :senha, 'u')");
         
@@ -49,16 +43,16 @@ if(isset($_POST['btn_cadastrar'])){
             $stmt->bindParam(':senha',$senha);
         
             if($stmt->execute()){
-
                 $idUser = $database->lastInsertId();
                                
                 $_SESSION['id'] = $idUser;
                 $_SESSION['permissao'] = 'u';
                 
                 header('location: ../../index.php');
-
             }
-        }else{$erros["EMAIL"] = "Valor do campo ja cadastrado";}
+        }else{
+            $erros["EMAIL"] = "Valor do campo ja cadastrado";
+        }
     }
 }
 
